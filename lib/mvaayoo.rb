49131a32828ed6e5647063d25d44d1b739598ad0
Kranthi Renamala <kranthi1027@users.noreply.github.com>
2017-08-31 13:32:11 +0530

    Update to remove encoding
    Trying to remove encoding

--- lib/mvaayoo.rb
+++ lib/mvaayoo.rb
@@ -3,8 +3,9 @@ require 'faraday'
 module Mvaayoo
   # send sms
   def self.send_message msg, receipient_no
-    message = ["00", msg.each_byte.map { |b| b.to_s(16) }.join("00")].join
-    http_conn '/mvaayooapi/MessageCompose', ["user=#{MVAAYOO_USER}:#{MVAAYOO_PASSWORD}", "receipientno=#{receipient_no}", "msgtype=4", "senderID=#{SENDER_ID}", "dcs=8", "msgtxt=#{message}", "ishex=1", "state=4", "msgtype=4", "ishex=1"] 
+  #  message = ["00", msg.each_byte.map { |b| b.to_s(16) }.join("00")].join
+    message = msg
+    http_conn '/mvaayooapi/MessageCompose', ["user=#{MVAAYOO_USER}:#{MVAAYOO_PASSWORD}", "receipientno=#{receipient_no}", "msgtype=4", "senderID=#{SENDER_ID}", "dcs=0", "msgtxt=#{message}", "ishex=1", "state=4", "msgtype=4", "ishex=1"] 
   end
  
   # check balance
